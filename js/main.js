e1b5fa8eff373cb44fa10ef275b919d77701eb58
Max Radke <89997459+Max-Radke@users.noreply.github.com>
2025-07-24 19:16:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-// Countdown bis zum 03.03.2026 mit Raketenanimation
+// Countdown bis zum Start
 function updateCountdown() {
     const countdownElement = document.getElementById("countdown");
-    const rocketElement = document.getElementById("rocket");
-    const launchDate = new Date("2026-03-03T00:00:00").getTime();
+    const launchDate = new Date("2025-03-03T00:00:00").getTime();
     const now = new Date().getTime();
     const distance = launchDate - now;
 
     if (distance < 0) {
         countdownElement.innerText = "🚀 Der Start ist erfolgt!";
-        rocketElement.classList.add("launch");
         return;
     }
 
